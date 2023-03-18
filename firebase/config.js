d8f92ab17150ce85adc6105ffbb08bc4e3ce41eb
Bashir Uddin <uddinbashir1999@gmail.com>
2023-03-18 15:29:39 +0500

    fix: add firebase configs

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzzgr5zC1BfjNp3XWKHfsIP2ijwGEBheQ",
+  authDomain: "gallery-94ac0.firebaseapp.com",
+  projectId: "gallery-94ac0",
+  storageBucket: "gallery-94ac0.appspot.com",
+  messagingSenderId: "919451018933",
+  appId: "1:919451018933:web:a5d3776e9b29ab345280fc",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);

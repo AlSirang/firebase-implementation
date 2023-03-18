8e81eaf681068a33384ee8cf75c797a1b1416986
Bashir Uddin <uddinbashir1999@gmail.com>
2023-03-18 15:30:48 +0500

    fix: add sign in page

---
+++ components/auth-form.js
@@ -0,0 +1,45 @@
+export default function authForm({ onSubmit = () => false, submitButtonText }) {
+  return (
+    <form className="mt-8 space-y-6" onSubmit={onSubmit}>
+      <div className="space-y-3 rounded-md shadow-sm">
+        <div>
+          <label htmlFor="email-address" className="sr-only">
+            Email address
+          </label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+            className="relative block w-full rounded-md border-0 py-3 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            required
+            className="relative block w-full rounded-md border-0 py-3 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          {submitButtonText}
+        </button>
+      </div>
+    </form>
+  );
+}
